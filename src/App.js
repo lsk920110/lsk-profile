e8dd836a2de55447f6ca41be8b9426f5a8bb6a5b
sklee-rowem <lsk@rowem.com>
2023-09-15 22:22:45 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Divider,
+  Grid,
+  Paper,
+  Tab,
+  Tabs,
+  Typography,
+} from "@mui/material";
+import "./App.css";
+import BasicTabs from "./BasicTabs";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper sx={{ margin: 10, padding: 5, marginBottom: 5 }}>
+        나의 프로필과 사진이 배치됨
+      </Paper>
+      <Box marginLeft={10} marginRight={10}>
+        <BasicTabs />
+      </Box>
+      <Box marginLeft={10} marginRight={10}>
+        <Grid container spacing={3}>
+          <Grid item xs={3}>
+            <Card>
+              <CardContent>
+                <Typography>Works</Typography>
+                <Typography>
+                  Fullstack Developer
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={6}>
+            <Card>
+              <CardContent>
+                <Typography>About</Typography>
+                <Typography>저는....</Typography>
+              </CardContent>
+              <CardContent>
+            <Divider />
+
+              </CardContent>
+            
+            
+              <CardContent>
+                <Typography>Skills</Typography>
+                <Typography>저는....</Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+          <Grid item xs={3}>
+            <Card>asd</Card>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   );
 }
